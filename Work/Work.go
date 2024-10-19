d48964aac0b35292fa70f8732ea35644d085779d
ThePiachu <ThePiachu@gmail.com>
2024-10-19 13:27:05 -0700

    Tweak

--- Work/Work.go
+++ Work/Work.go
@@ -3,7 +3,10 @@ package Work
 import (
 	"github.com/ThePiachu/Go/Log"
 	"golang.org/x/net/context"
+	gobcy "github.com/ThePiachu/gobcy/v2"
 )
+
+var BCY gobcy.API
 
 func Work(c context.Context) {
 	Log.Infof(c, "Work!")
